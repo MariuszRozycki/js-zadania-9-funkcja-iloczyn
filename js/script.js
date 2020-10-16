722e4abd05a419b39e2ee50f56381def348f8b92
mariansc83 <mariansc83@icloud.com>
2020-10-16 13:58:55 +0200

    Add 9-funkcja-iloczyn

---
+++ js/script.js
@@ -0,0 +1,15 @@
+// ES5
+// function multiply(num1, num2, num3) {
+//   let result = num1 * num2 * num3;
+
+//   return result;
+// }
+
+// let multiplyResult = multiply(2, 3, 4);
+// console.log(multiplyResult);
+
+// ES6
+const multiply = (num1, num2, num3) => num1 * num2 * num3;
+
+let multiplyResult = multiply(2, 3, 4);
+console.log(multiplyResult);
